a5ebc0941665b46450d6a911af0607ce9f14cd77
James McNeill <james.s.mcneill@gmail.com>
2018-11-09 19:52:23 -0800

    update python versions to 3.5 to 3.7d

--- setup.py
+++ setup.py
@@ -17,8 +17,6 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
