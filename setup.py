5765595867c089ef57b100115420486d37b53024
James McNeill <james.s.mcneill@gmail.com>
2018-11-15 07:09:57 -0800

    Update long description in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,22 @@ setup(
     name='sitka',
     version='0.1.2',
     description='A built environment analysis and modeling library for Python.',
-    long_description="""# A library for built environment modeling and simulation
-                        directly in Python.""",
+    long_description="""
+    Sitka is a built environment analysis and modeling library for Python.
+
+    This library is intended to make modeling and simulation for the built environment more hackable and easier to use for:
+
+    * Portable design tools
+    * Scalable web apps and services
+    * Quick design and analysis studies
+    * Interactive simulations and analyses
+    * Testing new models
+
+    The library is currently being written entirely in Python using [Pandas](https://pandas.pydata.org) as a core dependency in order to allow easier integration with data analysis in [Jupyter](https://jupyter.org).
+
+    A focus of this project is to create tools to make building models more hackable.  The library can be used to create a simple model just a few inputs or a larger whole building model can be created by connecting multiple objects.
+
+    """,
     long_description_content_type='text/markdown',
     url='https://github.com/mcneillj/sitka',
     author='James McNeill',
