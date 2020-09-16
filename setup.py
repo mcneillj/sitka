604890211d05def95cd7f7322225451677a146e6
James McNeill <james.mcneill@cedarstack.io>
2020-09-16 14:28:00 -0700

    Update readme and setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from io import open
 
 setup(
     name='sitka',
-    version='0.1.2',
+    version='0.1.3',
     description='A built environment analysis and modeling library for Python.',
     long_description="""
     Sitka is a built environment analysis and modeling library for Python.
